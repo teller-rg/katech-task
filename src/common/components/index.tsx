492b41a767f01f3d1223e0cfb0e9bdf12437ffc9
Teller Guluzade <tellerg@crocusoft.com>
2023-04-19 19:09:06 +0400

    feat:add general file for components

---
+++ src/common/components/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "./Typography";
+import { Row } from "./Row";
+import { Card } from "./Card";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { Container } from "./Container";
+
+export { Typography, Row, Card, Button, Input, Container };
