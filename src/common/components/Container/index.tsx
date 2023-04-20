6194ff8d51b64d3313f211be06fd0e0d49bba968
Teller Guluzade <tellerg@crocusoft.com>
2023-04-20 18:44:57 +0400

    fix:update Container component

--- src/common/components/Container/index.tsx
+++ src/common/components/Container/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Props } from "./models";
 
-export const Container: FC<Props> = ({ children, className }) => (
+export const Container: FC<Props> = ({ children, className = "" }) => (
   <div className={`${className} container`}>{children}</div>
 );
