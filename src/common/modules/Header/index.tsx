67f01255e5b055afb657cff066e429ae5969d091
Teller Guluzade <tellerg@crocusoft.com>
2023-04-20 14:30:40 +0400

    fix:update Header module

--- src/common/modules/Header/index.tsx
+++ src/common/modules/Header/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Container, Typography } from "common/components";
 
+import "./style.less";
+
 export const Header: FC = () => {
   return (
-    <header>
-      <Container>
-        <Typography.Title title="My Time Log" level="h1" />
+    <header className="header">
+      <Container className="d-flex align-center">
+        <Typography.Title title="My Time Log" level="h1" fontSize={32} />
       </Container>
     </header>
   );
